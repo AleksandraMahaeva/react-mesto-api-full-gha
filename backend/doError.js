212a9fc267fef09429e678acdaf7bcfaac9cee36
Alexsandra <aleksamahaeva@yandex.ru>
2023-04-28 18:18:37 +0300

    обнуление

--- backend/doError.js
+++
@@ -1,6 +0,0 @@
-module.exports.doError = (err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  const message = statusCode === 500 ? 'Произошла ошибка' : err.message;
-  res.status(statusCode).send({ message });
-  next(err);
-};
--- backend/errors/authorizError.js
+++
@@ -1,8 +0,0 @@
-class AuthorizError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-  }
-}
-
-module.exports = AuthorizError;
