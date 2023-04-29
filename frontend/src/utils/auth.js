719af07f8794e4b8369cf4e7ca6dd785b4a9a0dd
Alexsandra <aleksamahaeva@yandex.ru>
2023-04-29 21:08:02 +0300

    credentials: 'include'

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -20,6 +20,7 @@ export const register = (email, password) => {
 export const authorize = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
+    'credentials': 'include',
     headers: {
       "Content-Type": "application/json"
     },
@@ -30,6 +31,7 @@ export const authorize = (email, password) => {
 export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
+    credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
